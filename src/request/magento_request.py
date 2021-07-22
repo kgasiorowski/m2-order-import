d8c19d35f6bd06c9addc36059f6e72c3b0122475
Kuba Gasiorowski <kgasiorowski123@gmail.com>
2021-07-22 18:34:13 -0400

    Add request wrapper skeleton code

---
+++ src/request/magento_request.py
@@ -0,0 +1,31 @@
+import src.auth.secret as secret
+from src.model.Refund import Refund
+from src.model.Order import Order
+from src.model.Invoice import Invoice
+from src.model.Shipment import Shipment
+from requests.models import Response, Request
+
+class Request:
+    def __init__(self):
+        self.url = secret.url
+        self.rest_path = 'rest/default/V1/'
+
+
+    def buildRequestUrl(self) -> str:
+        return self.url + '/' if not self.url.endswith('/') else '' + self.rest_path
+
+
+    def createOrder(self, order: Order) -> Response:
+        ...
+
+
+    def createInvoice(self, order: Order, invoice: Invoice) -> Response:
+        ...
+
+
+    def createShipment(self, order: Order, shipment: Shipment) -> Response:
+        ...
+
+
+    def createRefund(self, order: Order, refund: Refund) -> Response:
+        ...
