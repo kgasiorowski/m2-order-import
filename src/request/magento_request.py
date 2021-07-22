c53b1fba425df9588181b366fc5cfbcd3c70c6f5
Kuba Gasiorowski <kgasiorowski123@gmail.com>
2021-07-22 18:42:19 -0400

    Spacing and naming fixes

--- src/request/magento_request.py
+++ src/request/magento_request.py
@@ -5,7 +5,8 @@ from src.model.Invoice import Invoice
 from src.model.Shipment import Shipment
 from requests.models import Response, Request
 
-class Request:
+
+class MagentoRequest:
     def __init__(self):
         self.url = secret.url
         self.rest_path = 'rest/default/V1/'
@@ -14,18 +15,14 @@ class Request:
     def buildRequestUrl(self) -> str:
         return self.url + '/' if not self.url.endswith('/') else '' + self.rest_path
 
-
     def createOrder(self, order: Order) -> Response:
         ...
-
 
     def createInvoice(self, order: Order, invoice: Invoice) -> Response:
         ...
 
-
     def createShipment(self, order: Order, shipment: Shipment) -> Response:
         ...
-
 
     def createRefund(self, order: Order, refund: Refund) -> Response:
         ...
