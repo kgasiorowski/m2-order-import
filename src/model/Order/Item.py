18974583fbbe7bcbf620496d3c6a39a82628d7e7
Kuba Gasiorowski <kgasiorowski123@gmail.com>
2021-07-29 15:11:33 -0400

    Add item data fields

--- src/model/Order/Item.py
+++ src/model/Order/Item.py
@@ -1,3 +1,8 @@
 class Item:
     def __init__(self):
-        ...
+        self.line_id = None
+        self.line_quantity = None
+        self.line_price = None
+        self.line_total = None
+        self.line_variant_sku = None
+        self.line_variant_weight = None
