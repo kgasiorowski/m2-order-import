37896a35569b210d40950b1d0dc15c660bd8da4e
Kuba Gasiorowski <kgasiorowski123@gmail.com>
2021-07-30 09:22:00 -0400

    Add some basic status reporting

--- src/main.py
+++ src/main.py
@@ -2,15 +2,24 @@ import src.io.csv as csv
 import src.const.const as const
 from src.request.magento_request import MagentoRequest
 import warnings
+import json
 
 
 def main():
-
     request = MagentoRequest()
 
     for order in csv.generateOrdersFromCsv(const.data_path + const.input_file_name):
         response = request.createOrder(order)
-        print(response.status_code)
+        if response.status_code == 200:
+            print(f"Order {order.name} successfully created")
+        else:
+            error_message = json.loads(response.content.decode())['message']
+            if 400 <= response.status_code < 500:
+                print(f"Order {order.name} could not be created, there was an error: "
+                      f"{response.reason} -> {error_message}")
+            elif response.status_code > 500:
+                print(f"Order {order.name} could not be created, there was a server error: "
+                      f"{response.reason} -> {error_message}")
 
 
 if __name__ == "__main__":
